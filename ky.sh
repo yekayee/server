9828f6c5c7c156e221ee03188df8e75e78dda322
Kaye <yekayee48@gmail.com>
2017-04-01 16:27:19 +0700

    Update ky.sh

--- ky.sh
+++ ky.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-white="\033[1;37m"
-grey="\033[0;37m"
-purple="\033[0;35m"
-red="\033[1;31m"
-green="\033[1;32m"
-yellow="\033[1;33m"
-Purple="\033[0;35m"
-Cyan="\033[0;36m"
-Cafe="\033[0;33m"
-Fiuscha="\033[0;35m"
-blue="\033[1;34m"
-transparent="\e[0m"
-
 clear
-echo -e $Fiuscha"# [1] Install Apache2	    -	  $blue[9] Install Subdomain            #"
+echo -e "# [1] Install Apache2	    -	  [9] Install Subdomain            #"
 echo -e "# [2] Install PHP5	    -     [10] Install FTP             	   #"
 echo -e "# [3] Install PHP7 	    -     [11] Install SSL  	   	   #"
 echo -e "# [4] Install MariaDB	    -     [12] Install NTP   	   	   #"
